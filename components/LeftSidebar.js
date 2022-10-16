9453d5bb5ff5e669310ebc53727dd6d8c3c89d00
Srikanth Kumar <wpsrikanth@gmail.com>
2022-10-16 14:55:52 +0530

    dynamically updates categories

--- components/LeftSidebar.js
+++ components/LeftSidebar.js
@@ -1,17 +1,46 @@
+const URL = "http://localhost:9191/posts";
+import axios from "axios";
+import { useEffect, useState } from "react";
+
 const LeftSidebar = () => {
+  const [categories, setCategories] = useState([]);
+
+  const getFlatCategories = (data) => {
+    const listOfCategories = [];
+    data.map((post) => {
+      listOfCategories = [...listOfCategories, post.category];
+    });
+    setCategories(Array.from(new Set(listOfCategories)));
+    console.log("listofcats", listOfCategories);
+  };
+  useEffect(() => {
+    axios
+      .get(URL)
+      .then((res) => {
+        getFlatCategories(res.data);
+      })
+      .catch((err) => console.log(err));
+  }, []);
   return (
     <div>
       <h3 className="text-slate-700 py-1 rounded-r-lg px-3 text-2xl">Spaces</h3>
-      <ul
-        className="flex flex-wrap ease-in list-none  rounded max-w-sm ml-2 "
-      >
-        <li className="px-2 m-2 my-1 py-1 bg-gray-400 rounded-full">DSA</li>
-        <li className="px-2 m-2 my-1 py-1 bg-indigo-400 rounded-full">ML</li>
-        <li className="px-2 m-2 my-1 py-1 bg-blue-400 rounded-full">NLP</li>
-        <li className="m-2 px-2 my-1 py-1 bg-yellow-400 rounded-full">CSE</li>
+      <ul className="flex flex-wrap ease-in list-none  rounded max-w-sm ml-2 ">
+        {/* <li className="px-2 m-2 my-1 py-1 bg-gray-400 rounded-full">DSA</li> */}
+        {categories.map((category, id) => {
+          return (
+            <li
+              key={id}
+              className="px-2 m-2 my-1 py-1 bg-indigo-400 rounded-full"
+            >
+              {category}
+            </li>
+          );
+        })}
+        {/* <li className="px-2 m-2 my-1 py-1 bg-blue-400 rounded-full">NLP</li>
+        <li className="m-2 px-2 my-1 py-1 bg-yellow-400 rounded-full">CSE</li> */}
       </ul>
     </div>
   );
 };
-  
+
 export default LeftSidebar;
