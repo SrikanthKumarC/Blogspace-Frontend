9e91883b93ad61e807f7ce47899f37246ef8f00a
Srikanth Kumar <wpsrikanth@gmail.com>
2022-10-16 14:38:55 +0530

    changes to spaces

--- components/LeftSidebar.js
+++ components/LeftSidebar.js
@@ -3,7 +3,7 @@ const LeftSidebar = () => {
     <div>
       <h3 className="text-slate-700 py-1 rounded-r-lg px-3 text-2xl">Spaces</h3>
       <ul
-        className="flex flex-wrap ease-in list-none px-3 py-2 p-5 rounded max-w-sm ml-2 "
+        className="flex flex-wrap ease-in list-none  rounded max-w-sm ml-2 "
       >
         <li className="px-2 m-2 my-1 py-1 bg-gray-400 rounded-full">DSA</li>
         <li className="px-2 m-2 my-1 py-1 bg-indigo-400 rounded-full">ML</li>
