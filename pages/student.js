9c4ceee8cebaef9236316b3bb348918f49f87988
Srikanth Kumar <wpsrikanth@gmail.com>
2022-11-26 12:01:34 +0530

    bugs

--- pages/student.js
+++ pages/student.js
@@ -13,7 +13,41 @@ const Student = () => {
   const [reload, setReload] = useState(false);
   const posts = usePosts(category, reload);
    
-
+  const showProfileData = () => {
+    if (status === "authenticated") {
+      return (
+        <div className="max-w-xl flex flex-col items-center justify-center">
+          <Image
+            src={session.user.image}
+            alt="profile"
+            width={32}
+            height={32}
+            className="rounded-full w-32 h-32"
+          />
+          <h1 className="text-2xl font-bold">{session.user.name}</h1>
+          <p className="text-xl">{session.user.email}</p>
+          <button
+            className="bg-red-500 text-white px-4 py-2 rounded mt-4"
+            onClick={() => signOut()}
+          >
+            Logout
+          </button>
+        </div>
+      );
+    } else {
+      return (
+        <div className="max-w-xl flex flex-col items-center justify-center">
+          <h1 className="text-2xl font-bold">You are not logged in</h1>
+          <button
+            className="bg-red-500 text-white px-4 py-2 rounded mt-4"
+            onClick={() => signIn()}
+          >
+            Login - Access to Students only
+          </button>
+        </div>
+      );
+    }
+  };
   return (
     <>
       <Nav student={true} />
